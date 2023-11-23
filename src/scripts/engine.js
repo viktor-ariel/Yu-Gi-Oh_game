02349bde1fec764b47eba1c0b378a354571e61a6
Viktor Ariel <98894099+viktor-ariel@users.noreply.github.com>
2023-11-22 22:53:39 -0300

    Add files via upload

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -2,7 +2,7 @@ const state= {
     score:{
         playerScore: 0,
         computerScore: 0,
-        scoreBox: document.getElementById("score_points"),
+        scoreBox: document.getElementById("score__points"),
     },
     cardSprites:{
         avatar: document.getElementById("card-image"),
